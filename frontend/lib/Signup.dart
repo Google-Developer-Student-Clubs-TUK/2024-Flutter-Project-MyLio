8968b041cef5c00de452a79e0c38ed547ab14362
seungwonHong <nonamed814@gmail.com>
2024-12-08 22:09:18 +0900

    수정됨

--- frontend/lib/Signup.dart
+++ frontend/lib/Signup.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:get/get.dart';
-import 'Login.dart';
 
 void main() async {
   runApp(Signup());
