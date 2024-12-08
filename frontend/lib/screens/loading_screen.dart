f6ff26eac639d07a9a0ac651cd7dd926f52beed4
IvyKi <kinahae0331@tukorea.ac.kr>
2024-12-08 21:59:23 +0900

    feature: 홈, 자기소개서 화면
     241208

---
+++ frontend/lib/screens/loading_screen.dart
@@ -0,0 +1,111 @@
+import 'dart:math';
+import 'question_result.dart';
+import 'app_colors.dart';
+import 'package:flutter/material.dart';
+
+
+
+// 로딩 화면
+class LoadingScreen extends StatefulWidget {
+  final String title;
+  final String companyName;
+  final String jobTitle;
+  final List<String> questions;
+
+  const LoadingScreen({
+    super.key,
+    required this.title,
+    required this.companyName,
+    required this.jobTitle,
+    required this.questions,
+  });
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // AnimationController 초기화 (2초 동안 반복)
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    )..repeat(); // 무한 반복
+
+    // 2초 후 결과 화면으로 이동
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => QuestionResult(
+            questions: widget.questions,
+
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // 메모리 누수를 방지하기 위해 dispose 호출
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // AnimatedBuilder로 애니메이션 적용
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2 * pi, // 0 ~ 2π 회전
+                  child: child,
+                );
+              },
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: 92,
+                height: 92,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 16),
+            ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return const LinearGradient(
+                  colors: [
+                    // Gradients.blue,
+                    Gradients.purple,
+                    Gradients.pink
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ).createShader(bounds);
+              },
+              child: const Text(
+                'Loading...',
+                style: TextStyle(
+                    fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ frontend/lib/widgets/bottom_bar.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import '../screens/app_colors.dart';
+
+class BottomBar extends StatelessWidget {
+  final VoidCallback onGridPressed;
+  final VoidCallback onSettingsPressed;
+  final VoidCallback onFabPressed;
+
+  const BottomBar({
+    Key? key,
+    required this.onGridPressed,
+    required this.onSettingsPressed,
+    required this.onFabPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        // BottomAppBar for navigation buttons
+        BottomAppBar(
+          // color: Colors.white,
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 6.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(
+                  Icons.dashboard,
+                  size: 24,
+                  color: Color(0xffA095C1),
+                ),
+                onPressed: onGridPressed,
+              ),
+              const SizedBox(width: 48), // 중앙 FloatingActionButton 공간
+              IconButton(
+                icon: const Icon(
+                  Icons.settings,
+                  size: 24,
+                  color: Color(0xff432C81),
+                ),
+                onPressed: onSettingsPressed,
+              ),
+            ],
+          ),
+        ),
+        // FloatingActionButton positioned in the center
+        // Positioned(
+        //   bottom: 40.0, // 버튼을 적절히 위로 배치
+        //   child: Container(
+        //     height: 70.0,
+        //     width: 70.0,
+        //     decoration: const BoxDecoration(
+        //       shape: BoxShape.circle,
+        //       gradient: LinearGradient(
+        //         colors: [
+        //           Gradients.blue,
+        //           Gradients.purple,
+        //           Gradients.pink,
+        //         ],
+        //         begin: Alignment.topLeft,
+        //         end: Alignment.bottomRight,
+        //       ),
+        //       boxShadow: [
+        //         BoxShadow(
+        //           color: Colors.black26,
+        //           blurRadius: 5,
+        //           offset: Offset(0, 3),
+        //         ),
+        //       ],
+        //     ),
+        //     child: FloatingActionButton(
+        //       shape: const CircleBorder(),
+        //       onPressed: onFabPressed,
+        //       elevation: 0,
+        //       highlightElevation: 0,
+        //       hoverElevation: 0,
+        //       focusElevation: 0,
+        //       backgroundColor: Colors.transparent,
+        //       child: const Icon(Icons.add, size: 41, color: Colors.white),
+        //     ),
+        //   ),
+        // ),
+      ],
+    );
+  }
+}
