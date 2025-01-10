f6f0972b1726d3d7bdb1bb2b794edcb58c5b3805
seojinJeon <gij0321@naver.com>
2025-01-10 13:59:12 +0900

    Design: add_resume에 이력서 추가 버튼 및 PopupMenu 버튼 수정

--- frontend/lib/screens/add_resume.dart
+++ frontend/lib/screens/add_resume.dart
@@ -1,42 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:frontend/screens/edit.dart';
 import 'package:frontend/screens/my_resume_create_page.dart';
 import 'package:get/get.dart';
 
-void main(){
+void main() {
   runApp(Add_Resume());
 }
 
-class Add_Resume extends StatefulWidget{
+class Add_Resume extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return Add_Resume_State();
   }
 }
 
-class Add_Resume_State extends State<Add_Resume>{
-  Widget build(BuildContext context){
+class Add_Resume_State extends State<Add_Resume> {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text('My 이력서', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700,),)
+        title: const Text(
+          'My 이력서',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+        ),
       ),
       backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.all(30),
+        padding: const EdgeInsets.all(30),
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                padding: EdgeInsets.only(left: 20,right: 20,top: 15,bottom: 15),
+                padding: const EdgeInsets.only(
+                    left: 20, right: 20, top: 15, bottom: 15),
                 width: 352,
                 height: 92,
                 decoration: BoxDecoration(
-                  color: Color.fromRGBO(251, 251, 251, 1),
+                  color: const Color.fromRGBO(251, 251, 251, 1),
                   borderRadius: BorderRadius.circular(16),
                 ),
                 child: Column(
@@ -49,94 +53,123 @@ class Add_Resume_State extends State<Add_Resume>{
                           width: 57.31,
                           height: 18,
                           decoration: BoxDecoration(
-                              color: Color.fromRGBO(144, 140, 255, 1),
+                            color: const Color.fromRGBO(144, 140, 255, 1),
                             borderRadius: BorderRadius.circular(50),
                           ),
-                          child: Text('대표 이력서', style: TextStyle(color: Colors.white, fontSize: 8, fontWeight: FontWeight.w500),),
+                          child: const Text(
+                            '대표 이력서',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8,
+                                fontWeight: FontWeight.w500),
+                          ),
                         ),
-                        Spacer(),
+                        const Spacer(),
                         PopupMenuButton(
                           color: Colors.white,
-                            elevation: 8,
-                            offset: Offset(0, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16)
-                            ),
-                            itemBuilder: (context){
-                              return <PopupMenuEntry<dynamic>>[
-                                PopupMenuItem(
-                                    child: Center(
-                                      child: Text('대표이력서 설정', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),),
-                                    ),
-                                  onTap: (){
-
-                                  },
+                          elevation: 1,
+                          offset: const Offset(0, 0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          itemBuilder: (context) {
+                            return <PopupMenuEntry<dynamic>>[
+                              PopupMenuItem(
+                                child: const Center(
+                                  child: Text(
+                                    '대표이력서 설정',
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w400),
+                                  ),
                                 ),
-                                const PopupMenuDivider(
-                                  height: 5,
+                                onTap: () {},
+                              ),
+                              const PopupMenuDivider(
+                                height: 5,
+                              ),
+                              PopupMenuItem(
+                                child: const Center(
+                                  child: Text('수정하기',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400)),
                                 ),
-                                PopupMenuItem(
-                                    child: Center(
-                                      child: Text('수정하기', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-                                    ),
-                                onTap: (){
-                                      Get.to(Edit());
+                                onTap: () {
+                                  Get.to(Edit());
                                 },
+                              ),
+                              const PopupMenuDivider(
+                                height: 5,
+                              ),
+                              PopupMenuItem(
+                                child: const Center(
+                                  child: Text('삭제하기',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400)),
                                 ),
-                                const PopupMenuDivider(
-                                  height: 5,
+                                onTap: () {},
+                              ),
+                              const PopupMenuDivider(
+                                height: 5,
+                              ),
+                              PopupMenuItem(
+                                child: const Center(
+                                  child: Text('복사하기',
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400)),
                                 ),
-                                PopupMenuItem(
-                                    child: Center(
-                                    child: Text('삭제하기', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),),
-                                  onTap: (){
-
-                                  },
-                                ),
-                                const PopupMenuDivider(
-                                  height: 5,
-                                ),
-                                PopupMenuItem(
-                                    child: Center(
-                                      child: Text('복사하기', style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400)),
-                                    ),
-                                  onTap: (){
-
-                                  },
-                                )
-                              ];
-                            },
-                          child: FaIcon(FontAwesomeIcons.ellipsisVertical, size: 15,),
+                                onTap: () {},
+                              )
+                            ];
+                          },
+                          child: const FaIcon(
+                            FontAwesomeIcons.ellipsisVertical,
+                            size: 15,
+                          ),
                         )
                       ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 10,
                     ),
-                    Text("서비스 기획자 이력서", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 14),)
+                    const Text(
+                      "서비스 기획자 이력서",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 14),
+                    )
                   ],
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              ElevatedButton(
-                  onPressed: (){
-                    Get.to(MyResumeCreatePage());
-                  },
-                  child: Text('+ 이력서 추가', style: TextStyle(fontSize: 14, color: Colors.grey),),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(352, 47),  // 너비 352, 높이 47
-                  backgroundColor: Colors.white,  // 배경 흰색
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.black,  // 테두리 색상
-                      width: 1,  // 테두리 두께
+              GestureDetector(
+                onTap: () {
+                  Get.to(MyResumeCreatePage());
+                },
+                child: DottedBorder(
+                  color: Colors.grey, // 점선 색상
+                  strokeWidth: 1, // 점선 두께
+                  dashPattern: [2, 2], // 점선 패턴
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(12), // 둥근 테두리
+                  child: Container(
+                    width: 352,
+                    height: 47,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      '+ 이력서 추가',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(12),
                   ),
-              )
-              )
+                ),
+              ),
             ],
           ),
         ),
