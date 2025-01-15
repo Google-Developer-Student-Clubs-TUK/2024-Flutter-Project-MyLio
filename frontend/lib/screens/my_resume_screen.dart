6f885e4cb745c8fdc8d5b68389ef6bb161ca61d5
seojinJeon <gij0321@naver.com>
2025-01-16 01:25:03 +0900

    Feat: 이력서 리스트 페이지 구현

--- frontend/lib/screens/my_resume_screen.dart
+++ frontend/lib/screens/my_resume_screen.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
-import 'package:frontend/screens/my_resume_create_page.dart';
+import 'package:frontend/screens/edit.dart';
+import 'package:get/get.dart';
+import 'my_resume_create_page.dart';
+import 'widgets/my_resume_empty_widget.dart';
+import 'widgets/resume_PopupMenu_Btn.dart';
 
-class MyResumeScreen extends StatelessWidget {
-  const MyResumeScreen({super.key});
+class MyResumeScreen extends StatefulWidget {
+  const MyResumeScreen({Key? key, required this.resumeTitle}) : super(key: key);
+
+  final String resumeTitle;
+
+  @override
+  State<MyResumeScreen> createState() => _MyResumeScreenState();
+}
+
+class _MyResumeScreenState extends State<MyResumeScreen> {
+  List<Map<String, dynamic>> resumes = []; // 이력서 데이터 리스트
+
+  void addResume(
+    String newResumeTitle,
+    List<String> industries,
+    String jobDuty,
+    List<String> capabilities,
+    List<String> strengths,
+    List<String> weaknesses,
+    List<Map<String, String>> activityExperience,
+    List<Map<String, String>> awards,
+    List<Map<String, String>> certificates,
+    List<Map<String, String>> languages,
+  ) {
+    setState(() {
+      resumes.add({
+        'title': newResumeTitle,
+        'industries': industries,
+        'jobDuty': jobDuty,
+        'capabilities': capabilities,
+        'strengths': strengths,
+        'weaknesses': weaknesses,
+        'activityExperience': activityExperience,
+        'awards': awards,
+        'certificates': certificates,
+        'languages': languages,
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,86 +72,265 @@ class MyResumeScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        width: double.infinity, // 부모 컨테이너의 너비를 화면 전체로 설정
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, // 세로 방향 상단 정렬
-          crossAxisAlignment: CrossAxisAlignment.center, // 가로 방향 중앙 정렬
-          children: [
-            const SizedBox(height: 20), // 상단 여백
-            GestureDetector(
-              onTap: () {
-                // 이력서 등록 화면으로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const MyResumeCreatePage(),
+      // 이력서가 없을 때 화면
+      body: resumes.isEmpty
+          ? MyResumeEmptyWidget(
+              onResumeAdded: (
+                title,
+                industries,
+                jobDuty,
+                capabilities,
+                strengths,
+                weaknesses,
+                activityExperience,
+                awards,
+                certificates,
+                languages,
+              ) {
+                addResume(
+                    title,
+                    industries,
+                    jobDuty,
+                    capabilities,
+                    strengths,
+                    weaknesses,
+                    activityExperience,
+                    awards,
+                    certificates,
+                    languages);
+              },
+            )
+          : _buildResumeList(context),
+    );
+  }
+
+  // 이력서가 있을 때 화면
+  Widget _buildResumeList(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: resumes.length,
+              itemBuilder: (context, index) {
+                final resume = resumes[index];
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 16),
+                  padding: const EdgeInsets.all(16),
+                  width: 352,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(251, 251, 251, 1),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            width: 57.31,
+                            height: 18,
+                            decoration: BoxDecoration(
+                              color: index == 0
+                                  ? const Color.fromRGBO(144, 140, 255, 1)
+                                  : Colors.grey[200],
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: Text(
+                              index == 0 ? '대표 이력서' : '이력서',
+                              style: TextStyle(
+                                color: index == 0 ? Colors.white : Colors.black,
+                                fontSize: 8,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          ResumePopupMenuBtn(
+                            onSetRepresentative: () {
+                              setState(() {
+                                final selectedResume = resumes.removeAt(index);
+                                resumes.insert(0, selectedResume);
+                              });
+                            },
+                            onEdit: () {
+                              Get.to(() => const Edit());
+                            },
+                            onDelete: () {
+                              setState(() {
+                                resumes.removeAt(index);
+                              });
+                            },
+                            onDuplicate: () {
+                              setState(() {
+                                resumes.insert(
+                                    index + 1, Map.from(resumes[index]));
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        resume['title'],
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      if (resume['industries'] != null &&
+                          resume['industries'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '산업군: ${resume['industries'].join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['jobDuty'] != null &&
+                          resume['jobDuty'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '직무: ${resume['jobDuty']}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['capabilities'] != null &&
+                          resume['capabilities'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '역량: ${resume['capabilities'].join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['strengths'] != null &&
+                          resume['strengths'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '강점: ${resume['strengths'].join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['weaknesses'] != null &&
+                          resume['weaknesses'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '약점: ${resume['weaknesses'].join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['activityExperience'] != null &&
+                          resume['activityExperience'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '활동/경험: ${resume['activityExperience'].map((activity) => activity['activityName']).join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['awards'] != null &&
+                          resume['awards'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '수상경력: ${resume['awards'].map((award) => award['title']).join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['certificates'] != null &&
+                          resume['certificates'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '자격증: ${resume['certificates'].map((certificate) => certificate['name']).join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                      if (resume['languages'] != null &&
+                          resume['languages'].isNotEmpty) ...[
+                        const SizedBox(height: 5),
+                        Text(
+                          '어학: ${resume['languages'].map((language) => language['language']).join(', ')}',
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ],
                   ),
                 );
               },
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.85,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: Colors.grey[400]!,
-                    width: 1.0,
-                  ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyResumeCreatePage(),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.85,
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 10),
-                      child: DottedBorder(
-                        color: Colors.grey[300]!,
-                        strokeWidth: 1,
-                        borderType: BorderType.RRect,
-                        radius: const Radius.circular(12),
-                        dashPattern: [6, 3],
-                        child: Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Column(
-                            children: [
-                              const Icon(
-                                Icons.description_outlined,
-                                size: 50,
-                                color: Colors.grey,
-                              ),
-                              const SizedBox(height: 16),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Text(
-                                    '이력서 등록하러 가기',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.black54,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  const SizedBox(width: 8),
-                                  const Icon(
-                                    Icons.chevron_right,
-                                    color: Colors.grey,
-                                    size: 30,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
+              );
+              if (result != null && result is Map<String, dynamic>) {
+                addResume(
+                  result['title'],
+                  result['industries'],
+                  result['jobDuty'],
+                  result['capabilities'],
+                  result['strengths'],
+                  result['weaknesses'],
+                  result['activityExperience'],
+                  result['awards'],
+                  result['certificates'],
+                  result['languages'],
+                );
+              }
+            },
+            child: DottedBorder(
+              color: Colors.grey,
+              strokeWidth: 1,
+              dashPattern: [2, 2],
+              borderType: BorderType.RRect,
+              radius: const Radius.circular(12),
+              child: Container(
+                width: 352,
+                height: 47,
+                alignment: Alignment.center,
+                child: const Text(
+                  '+ 이력서 추가',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
