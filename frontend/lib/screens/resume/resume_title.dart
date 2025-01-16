88ddb3884c3e22a2a9add1a749d81c3d18705713
seojinJeon <gij0321@naver.com>
2025-01-17 03:42:54 +0900

    Design: 이력서 제목 페이지 디자인 수정

--- frontend/lib/screens/resume/resume_title.dart
+++ frontend/lib/screens/resume/resume_title.dart
@@ -77,14 +77,14 @@ class Resume_Title_State extends State<Resume_Title> {
                               color: Color(0xFFCCCCCC), width: 1.0),
                           borderRadius: BorderRadius.circular(12.0),
                         ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Color(0xFFCCCCCC), width: 2.0),
-                          borderRadius: BorderRadius.circular(12.0),
-                        ),
                         enabledBorder: OutlineInputBorder(
                           borderSide: const BorderSide(
                               color: Color(0xFFCCCCCC), width: 1.0),
+                          borderRadius: BorderRadius.circular(12.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Color(0xFF908CFF), width: 1.0),
                           borderRadius: BorderRadius.circular(12.0),
                         ),
                       ),
