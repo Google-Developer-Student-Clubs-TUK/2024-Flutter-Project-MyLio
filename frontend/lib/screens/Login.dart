ca9ed94c0692018b83d9332b196cd07cab01cf88
1233day <1233day@naver.com>
2025-02-01 02:28:13 +0900

    fix: login ui 복구

--- frontend/lib/screens/Login.dart
+++ frontend/lib/screens/Login.dart
@@ -22,6 +22,8 @@ class Login extends StatefulWidget {
 
 class LoginState extends State<Login> {
   final _formkey = GlobalKey<FormState>();
+  late SharedPreferences sharedPreferences;
+  bool logincheck = false;
   final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
   bool _isPasswordVisible = false;
 
@@ -98,48 +100,139 @@ class LoginState extends State<Login> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Spacer(),
-            Text(
-              '로그인',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-            ),
-            SizedBox(height: 30),
-            Form(
-              key: _formkey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(hintText: "이메일"),
-                    onChanged: (value) => email = value,
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    obscureText: !_isPasswordVisible,
-                    decoration: InputDecoration(
-                      hintText: "비밀번호",
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isPasswordVisible = !_isPasswordVisible;
-                          });
-                        },
-                        icon: Icon(_isPasswordVisible ? Icons.visibility : Icons.visibility_off),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '로그인',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(height: 30),
+                Form(
+                  key: _formkey,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 323,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "이메일",
+                            hintStyle: TextStyle(fontSize: 12),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            email = value; // 이메일 값 저장
+                          },
+                        ),
                       ),
-                    ),
-                    onChanged: (value) => password = value,
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: 323,
+                        child: TextFormField(
+                          obscureText: !_isPasswordVisible,
+                          decoration: InputDecoration(
+                              hintText: "비밀번호",
+                              hintStyle: TextStyle(fontSize: 12),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                              ),
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isPasswordVisible = !_isPasswordVisible;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _isPasswordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    color: Color(0xFFC1C7D0),
+                                  ))),
+                          onChanged: (value) {
+                            password = value; // 비밀번호 값 저장
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      TextButton(
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            login(); // 로그인 함수 호출
+                          }
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Color(0xFF878CEF),
+                          minimumSize: Size(352, 45),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: Text(
+                          "로그인",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      GestureDetector(
+                        child: Text(
+                          '비밀번호를 잊어버리셨나요?',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Color(0xFF888888),
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 40),
-                  TextButton(
-                    onPressed: () {
-                      if (_formkey.currentState!.validate()) {
-                        login();
-                      }
-                    },
-                    child: Text("로그인"),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
             Spacer(),
+            Center(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '아직 계정이 없으신가요? ',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color(0xFFAAAAAA),
+                          ),
+                        ),
+                        GestureDetector(
+                          child: Text(
+                            '회원가입',
+                            style:
+                            TextStyle(color: Color(0xFF878CEF), fontSize: 14),
+                          ),
+                          onTap: () {
+                            Get.to(() => Signup());
+                          },
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      child: Text(
+                        '건너뛰기',
+                        style: TextStyle(fontSize: 12, color: Color(0xFFCCCCCC)),
+                      ),
+                      onTap: () {
+                        Get.to(() => HomeScreen());
+                      },
+                    )
+                  ],
+                )),
           ],
         ),
       ),
